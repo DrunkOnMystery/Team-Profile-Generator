e98674be2b8403b689b46f6d507c7fcb43256fb2
Ben Brumby <bbrumby00@gmail.com>
2020-10-05 22:11:37 -0400

    rendering works and overwrites successfully. Beginning to comment out on files

--- Develop/app.js
+++ Develop/app.js
@@ -1,4 +1,5 @@
-const Employee = require("./lib/Employee");
+//Declare constants for required files
+
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -6,19 +7,23 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 const render = require("./lib/htmlRenderer");
-// const renderTeamMembers = render.promisify(fs.writeFile);
 
+//Declare constants for output for rendered file
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 
-
+//create arrays for team member objects and for ID numbers
 var teamMembers = [];
 var idArray = [];
 
+
+//create primary function
 function mainMenu() {
     idArray = [];
     teamMembers = [];
+
+    //create function for creating first team member, the manager, using inquirer prompts
     function createManager() {
         console.log("Please build your team");
         inquirer.prompt([{
@@ -30,7 +35,6 @@ function mainMenu() {
                     return true;
                 }
                 return "Please enter a name"
-                // .includes
             }
         },
         {
@@ -62,6 +66,7 @@ function mainMenu() {
 
         },
 
+        //Once the manager is done, push the Manager to the team member array, their ID number to the ID array, and start the nextTeamMember function
         ]).then(answer => {
             const manager = new Manager(answer.managerName, answer.managerID, answer.managerEmail, answer.managerOffice);
             teamMembers.push(manager);
@@ -70,6 +75,8 @@ function mainMenu() {
 
         })
     }
+
+    //Prompt the user to choose the next team member they want to create, and call the appropriate function based on their choice
     {
         function nextTeamMember() {
             inquirer.prompt([{
@@ -97,6 +104,7 @@ function mainMenu() {
         }
     }
 
+    //show the function for creating an Engineer. Function ends by re-calling the nextTeamMember function
     function createEngineer() {
         console.log("Please describe your engineer");
         inquirer.prompt([{
@@ -140,6 +148,7 @@ function mainMenu() {
         })
     }
 
+        //show the function for creating an Intern. Function ends by re-calling the nextTeamMember function
     function createIntern() {
         console.log("Please describe your intern");
         inquirer.prompt([{
@@ -177,6 +186,8 @@ function mainMenu() {
 
         })
     }
+
+//Function for rendering the team to the output file, once the user prompts to do so    
     {
         function renderTeam() {
             const renderedTeam = render(teamMembers)
@@ -184,7 +195,7 @@ function mainMenu() {
                 if (err) return console.log(err)
             });
 
-            //console log notification to make 
+            //console log notification to make once everything is rendered properly
             console.log("Successfully wrote to output path");
         }
     }
@@ -194,27 +205,3 @@ createManager();
 }
 
 mainMenu();
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
